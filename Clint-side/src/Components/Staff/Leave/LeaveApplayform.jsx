b660c12fb6b583186e8ef09cac4a78e7ff243293
sridhar-hp <sridhark3773@gmail.com>
2025-11-08 12:30:45 +0530

    staff leave applay is done

--- Clint-side/src/Components/Staff/Leave/LeaveApplayform.jsx
+++ Clint-side/src/Components/Staff/Leave/LeaveApplayform.jsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./LeaveApplayform.css";
+
 function LeaveApplayform() {
+    const [leaveApplayForm, setLeaveApplayForm] = useState({});
+
+    const handleLeaveApplay = async(e) => {
+        e.preventDefault(); 
+        window.location.reload();
+
+
+        try{
+            const res = await axios.post("http://localhost:8081/Leaveapply", leaveApplayForm);
+
+            if(res.data.success){
+                alert("Leave applied successfully but the status is pendign.If your leave apprved we will notify you via email and message.");
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
+    
+    }
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setLeaveApplayForm((prev) => ({ ...prev, [name]: value }));
+
+    };
+
     return (
         <>
             <div className="leave-form-page">
                 <h1 >LEAVE APPLY FORM</h1>
-                <form action="">
-                
+                <form onSubmit={handleLeaveApplay}>
+
 
                     <div className="lafn">
                         <label htmlFor=""> 1.Enter your name:</label>
-                        <input className="la" type="text" name="" id="" placeholder="" />
+                        <input className="la" type="text" name="name" id="" placeholder="" onChange={handleInput} required/>
                     </div>
 
                     <div className="lafi">
                         <label htmlFor=""> 2.Enter your ID:</label>
-                        <input className="la" type="text" name="" id="" placeholder="" />
+                        <input className="la" type="text" name="StaffId" id="" placeholder="" onChange={handleInput} required/>
                     </div>
 
                     <div className="lafi">
                         <label htmlFor=""> 3.Enter your Phone NO:</label>
-                        <input className="la" type="text" name="" id="" placeholder="" />
+                        <input className="la" type="text" name="PhoneNo" id="" placeholder="" onChange={handleInput} required/>
                     </div>
-                    
+
                     <div className="lafd">
                         <label htmlFor="">4.Enter your Department:</label>
-                        <select>
+                        <select name="Department" onChange={handleInput} required>
                             <option value="">-- Select Department --</option>
                             <option value="CSE">CSE</option>
                             <option value="IT">IT</option>
@@ -39,17 +68,12 @@ function LeaveApplayform() {
 
                     <div className="lafei">
                         <label htmlFor=""> 5.Email ID:</label>
-                        <input className="la" type="email" name="" id="" placeholder="" />
+                        <input className="la" type="email" name="EmailId" id="" placeholder="" required onChange={handleInput}/>
                     </div>
 
-                    
-
-                    
-
-                   
                     <div className="laft">
                         <label htmlFor="">6.leave type:</label>
-                        <select>
+                        <select name="Leavetype" onChange={handleInput} required>
                             <option value="">-- Select Leave Type --</option>
                             <option value="Sick Leave">Sick Leave</option>
                             <option value="Casual Leave">Casual Leave</option>
@@ -59,27 +83,27 @@ function LeaveApplayform() {
                         </select>
                     </div>
 
-                    <div className="lafad">
+                    {/* <div className="lafad">
                         <label htmlFor=""> 7.Applay Date:</label>
-                        <input className="la" type="date" name="" id="" placeholder="" />
-                    </div>
-                
+                        <input className="la" type="date" name="created_at" required id="" placeholder="" onChange={handleInput}/>
+                    </div> */}
+
                     <div className="laffd">
-                        <label  htmlFor="">8.From date:</label>
-                        <input className="la" type="date" name="" id="" placeholder="From date:" />
+                        <label htmlFor="">7.From date:</label>
+                        <input className="la" type="date" name="FromDate" id="" required placeholder="From date:" onChange={handleInput} />
                     </div>
-              
+
                     <div className="laftd">
-                        <label htmlFor="">9.To date:</label>
-                        <input className="la" type="date" name="" id="" placeholder="To date:" />
+                        <label htmlFor="">8.To date:</label>
+                        <input className="la" type="date" name="ToDate" id="" required placeholder="To date:" onChange={handleInput}/>
                     </div>
-               
+
                     <div className="lafr">
-                        <label htmlFor="">10.Reason:</label>
-                        <textarea type="text" name="" id="" placeholder="" />
+                        <label htmlFor="">9.Reason:</label>
+                        <textarea type="text" name="Reason" id="" placeholder="" required onChange={handleInput}/>
                     </div>
-            
-                    <button className="lafs">Submit</button>
+
+                    <button className="lafs">applay leave</button>
                 </form>
             </div>
         </>
