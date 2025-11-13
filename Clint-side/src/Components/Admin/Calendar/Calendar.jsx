07da49ed6e68984ec33c06741c5afcb93115844f
sridhar-hp <sridhark3773@gmail.com>
2025-11-13 10:57:58 +0530

    v1.1

--- Clint-side/src/Components/Admin/Calendar/Calendar.jsx
+++ Clint-side/src/Components/Admin/Calendar/Calendar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { FaRegCalendarAlt } from "react-icons/fa";
+import "./Calendar.css";
 
 function Calendar() {
-    return (
-        <div className="comingsoon-container-c">
-            <div className="comingsoon-content-c">
-                <FaRegCalendarAlt className="comingsoon-icon-c" />
-                <h2>Coming Soon</h2>
-                <p>This section is under development</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="comingsoon-container-c">
+      <div className="comingsoon-content-c">
+        <FaRegCalendarAlt className="comingsoon-icon-c" />
+        <h2>Coming Soon</h2>
+        <p>This section is under development</p>
+      </div>
+    </div>
+  );
 }
+
 export default Calendar;
