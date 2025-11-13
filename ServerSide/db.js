2b2115a8cb445ebb9bc64b6e847e726237868377
sridhar-hp <sridhark3773@gmail.com>
2025-11-13 15:26:18 +0530

    ui

--- ServerSide/db.js
+++ ServerSide/db.js
@@ -2,23 +2,23 @@ require('dotenv').config(); // load .env variables
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+    }
 );
 
 (async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Sequelize MySQL Connected");
-  } catch (err) {
-    console.error("❌ Unable to connect to DB:", err);
-  }
+    try {
+        await sequelize.authenticate();
+        console.log("✅ Sequelize MySQL Connected");
+    } catch (err) {
+        console.error("❌ Unable to connect to DB:", err);
+    }
 })();
 
 module.exports = sequelize;
